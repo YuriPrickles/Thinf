9590b7137428d54aa10b792de40818f2435e6054
LawnmowerKing <78399964+LawnmowerKing@users.noreply.github.com>
2021-09-06 08:29:58 +0800

    uploaded files

---
+++ Backgrounds/ChestWastelandBgStyle.cs
@@ -0,0 +1,55 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Thinf;
+
+namespace ExampleMod.Backgrounds
+{
+	public class ChestWastelandBgStyle : ModSurfaceBgStyle
+	{
+		public override bool ChooseBgStyle()
+		{
+			return !Main.gameMenu && Main.LocalPlayer.GetModPlayer<MyPlayer>().ZoneChestWasteland;
+		}
+
+		// Use this to keep far Backgrounds like the mountains.
+		public override void ModifyFarFades(float[] fades, float transitionSpeed)
+		{
+			for (int i = 0; i < fades.Length; i++)
+			{
+				if (i == Slot)
+				{
+					fades[i] += transitionSpeed;
+					if (fades[i] > 1f)
+					{
+						fades[i] = 1f;
+					}
+				}
+				else
+				{
+					fades[i] -= transitionSpeed;
+					if (fades[i] < 0f)
+					{
+						fades[i] = 0f;
+					}
+				}
+			}
+		}
+
+		public override int ChooseFarTexture()
+		{
+			return mod.GetBackgroundSlot("Backgrounds/ChestWastelandFar");
+		}
+
+		private static int SurfaceFrameCounter;
+		private static int SurfaceFrame;
+		public override int ChooseMiddleTexture()
+		{
+			return mod.GetBackgroundSlot("Backgrounds/ChestWastelandMid");
+		}
+
+		public override int ChooseCloseTexture(ref float scale, ref double parallax, ref float a, ref float b)
+		{
+			return mod.GetBackgroundSlot("Backgrounds/ChestWastelandClose");
+		}
+	}
+}
